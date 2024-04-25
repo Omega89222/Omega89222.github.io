f0f58f70b37e4aba0873d4d0293799a6392edc7e
Omega89222 <166948868+Omega89222@users.noreply.github.com>
2024-04-25 19:20:53 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('categories').addEventListener('change', function() {
+  var selectedCategory = this.value;
+  console.log("Catégorie sélectionnée : " + selectedCategory);
+  // Ajoute ici le code pour réagir au changement de catégorie sélectionnée
+});
